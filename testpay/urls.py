0684ba11f57725b76c0739a40670c4efb4865ce1
zhyun <like4m@naver.com>
2022-07-22 18:25:15 +0900

    second

--- testpay/urls.py
+++ testpay/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 app_name = 'testpay'
 urlpatterns = [
-    path('main/', views.testMain.as_view(), name='testmain'),
+    path('', views.testMain.as_view(), name='testMain'),
+    path('pc2app/', views.testPc2App.goTestPc2App, name='testPc2App'),
+
 ]
--- testpay/views.py
+++ testpay/views.py
@@ -1,5 +1,13 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 
 class testMain(TemplateView):
     template_name = 'testpay/testmain.html'
+
+
+class testPc2App(TemplateView):
+    template_name = 'testpay/testpc2app.html'
+
+    def goTestPc2App(request):
+        return render(request, 'testpay/testpc2app.html')
